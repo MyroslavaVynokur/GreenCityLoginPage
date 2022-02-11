b29000ef5d62287b8a4ad3c7ce9ad74c0dcc68e2
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-02-11 14:13:52 +0200

    initial commit

---
+++ src/main/java/LoginData.java
@@ -0,0 +1,73 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginData {
+
+    private WebDriver driver;
+
+    @FindBy(how = How.XPATH, using = "//a[@class = 'close-modal-window']/img")
+    private WebElement close;
+
+    @FindBy(how = How.XPATH, using = "//input[@id = 'email']")
+    private WebElement emailField;
+    @FindBy(how = How.XPATH, using = "//input[@id = 'password']")
+    private WebElement passwordField;
+    @FindBy(how = How.XPATH, using = "//button[@class = 'ubs-primary-global-button']")
+    private WebElement loginButton;
+    @FindBy(how = How.XPATH, using = "//button[@class = 'google-sign-in']")
+    private WebElement signInGoogle;
+    @FindBy(how = How.XPATH, using = "//span[@class = 'show-hide-btn']")
+    private WebElement showHidePassword;
+    @FindBy(how = How.XPATH, using = "//div[@class = 'forgot-wrapper']/a[@class = 'ubs-forgot-password']")
+    private WebElement forgotPassword;
+    @FindBy(how = How.XPATH, using = "//div[@id = 'email-err-msg']/app-error/div")
+    private WebElement errorEmail;
+    @FindBy(how = How.XPATH, using = "//div[@id = 'pass-err-msg']/app-error/div")
+    private WebElement errorPassword;
+    @FindBy(how = How.XPATH, using = "//div[@class = 'missing-account']/p/a[@class = 'ubs-sign-up-link']")
+    private WebElement signUpLink;
+    public LoginData(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //("click on 'close' button")
+    public LoginData clickCloseIcon() {
+        close.click();
+        return this;
+    }
+
+    //input email | value = {emailInput}
+    public LoginData inputEmail(String emailInput) {
+
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(emailInput, Keys.ENTER);
+        return this;
+    }
+
+    //input password | value = {passwordInput}
+    public LoginData inputPassword(String passwordInput) {
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(passwordInput, Keys.ENTER);
+        return this;
+    }
+
+    // click on login button
+    public LoginData clickLoginButton () {
+        loginButton.click();
+        return this;
+    }
+
+    //click on 'show-hide password' button
+    public LoginData clickShowHidePassword() {
+        showHidePassword.click();
+        return this;
+    }
+
+}
---
+++ src/test/java/LoginDataTest.java
@@ -0,0 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import java.time.Duration;
+
+import static org.junit.Assert.*;
+
+public class LoginDataTest {
+    private WebDriver driver;
+
+    final String BASE_URL = "https://ita-social-projects.github.io/GreenCityClient/#/";
+
+    @Before
+    public void beforeMethod() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(BASE_URL);
+        driver.findElement(By.partialLinkText("Увійти")).click();
+
+    }
+
+    @Test
+    public void logintest() {
+        LoginData login = new LoginData(driver);
+        login.inputEmail("mirka.vyno@gmail.com").inputPassword("Test-User123").clickLoginButton();
+        String actual = driver.getCurrentUrl();
+        String expected = "https://ita-social-projects.github.io/GreenCityClient/#/ubs/order";
+        Assert.assertEquals(actual, expected);
+
+    }
+
+
+//    @After
+//    public void AfterMethod() {
+//        driver.quit();
+//    }
+
+}
